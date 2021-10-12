aca9f360b68bfed48ef5533e1b5b0cfc95b2a9b0
giorgos kalomitsinis <80779522+georgios-kalomitsinis@users.noreply.github.com>
2021-10-12 22:49:23 +0300

    Update index_calculation.py

--- index_calculation.py
+++ index_calculation.py
@@ -1,4 +1,3 @@
-# -- Import the necessary libraries -- #
 import os
 import numpy as np
 import cv2
@@ -41,13 +40,13 @@ class Indexes:
 	# -- Identification of the the vegetation_indices -- #
 	# -- This module is able to extract only VARI, GLI, NGRDI and NGBDI maps -- #
 	def get_index(self, index_name):
-		if index_name == 'vari':
+		if index_name == 'VARI':
 			return self.VARI()
-		elif index_name == 'gli':
+		elif index_name == 'GLI':
 			return self.GLI()
-		elif index_name == 'ngrdi':
+		elif index_name == 'NGRDI':
 			return self.NGRDI()
-		elif index_name == 'ngbdi':
+		elif index_name == 'NGBDI':
 			return self.NGBDI()
 		else:
 			print('Uknown index')
@@ -71,46 +70,58 @@ img = img_4ch[:, :, :3].astype(np.float)
 img[img_4ch[:, :, 3] == 0] = np.nan
 empty_space = img_4ch[:, :, 3] == 0
 
+Idx = Indexes(img)
+
+# -- Appropriate messages in order to extract the desired vegetation index results -- # 
+# -- Select the vegetation index with its acronyms -- #
+index_name = input("Please enter the acronym of the Vegetation Index. The available VIs are:\n\
+\n1. Visible Atmospheric Resistant Index (VARI)\n\
+2. Green Leaf Index (GLI) \n\
+3. Normalized Green Red Difference Index (NGRDI) \n\
+4. Normalized Green Blue Difference Index (NGBDI)\n\
+--> ")
+
+# -- Check the input of the user -- #
+if index_name.isupper() == False:
+	index_name = index_name.upper()
+else:
+	index_name = index_name
+
+available_indices = ['VARI', 'GLI', 'NGRDI', 'NGBDI']
+
+if index_name not in available_indices:
+	print('Not availabe Vegetation Index')
+
 # -- Print function for testing reasons -- #
 print('Processing image with shape {} x {}'.format(img.shape[0], img.shape[1]))
 
-
-Idx = Indexes(img)
-index_names = ['vari', 'ngbdi', 'gli', 'ngrdi']
-
-
-for index_name in index_names:
-
-	# -- Calculate index -- #
-	index = Idx.get_index(index_name)
-
-	# -- Print functions for testing reasons -- #
-	print('Mean: {}'.format(np.mean(index_clear)))
-        print('Median: {}'.format(np.median(index_clear)))
+# -- Calculate index -- #
+index = Idx.get_index(index_name)
 
 
-	index_clear = index[~np.isnan(index)]
+index_clear = index[~np.isnan(index)]
 
-	# -- Calculate index histogram -- #
-	perc, edges, _ = plt.hist(index_clear, bins=100, range=(-1, 1), color='darkcyan', edgecolor='black')
+# -- Calculate index histogram -- #
+perc, edges, _ = plt.hist(index_clear, bins=100, range=(-1, 1), color='darkcyan', edgecolor='black')
 
-	# -- Find the real min, max values of the vegetation_index -- #
-	lower, upper = find_real_min_max(perc, edges, index_clear)
+# -- Find the real min, max values of the vegetation_index -- #
+lower, upper = find_real_min_max(perc, edges, index_clear)
 
-	# -- Plot and save the vegetation_index -- #
-	f = plt.figure()
-	f.set_figheight(index.shape[0]/f.get_dpi())
-	f.set_figwidth(index.shape[1]/f.get_dpi())
-	ax = plt.Axes(f, [0., 0., 1., 1.])
-	ax.set_axis_off()
-	f.add_axes(ax)
-	ax.imshow(np.clip(index, lower, upper), cmap=cmap, aspect='auto')
-	f.savefig('{}/{}.png'.format(save_dir, index_name), transparent=True)
-	plt.close()
+# -- Plot and save the vegetation_index -- #
+f = plt.figure()
+f.set_figheight(index.shape[0]/f.get_dpi())
+f.set_figwidth(index.shape[1]/f.get_dpi())
+ax = plt.Axes(f, [0., 0., 1., 1.])
+ax.set_axis_off()
+f.add_axes(ax)
+ax.imshow(np.clip(index, lower, upper), cmap='RdYlGn', aspect='auto')
+f.savefig('{}/{}.png'.format(save_dir, index_name), transparent=True)
+plt.close()
 
-	# -- The *.npy files are useful for the Problematic Areas Detection module -- # 
-	index_clipped = np.clip(index, lower, upper)
-	np.save('{}/{}_clipped.npy'.format(save_dir, index_name), index_clipped)
+
+# -- The *.npy files are useful for the Problematic Areas Detection module -- # 
+index_clipped = np.clip(index, lower, upper)
+np.save('{}/{}_clipped.npy'.format(save_dir, index_name), index_clipped)
 
 # -- Print function for testing reasons -- #	
 print('Done!')
