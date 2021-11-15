97dabe8739bed3bc9cc1d9fd14baad1121f12c68
giorgos kalomitsinis <80779522+georgios-kalomitsinis@users.noreply.github.com>
2021-11-15 16:47:27 +0200

    Update index_calculation.py

--- index_calculation.py
+++ index_calculation.py
@@ -4,9 +4,12 @@ import cv2
 import matplotlib.pyplot as plt
 import argparse
 import textwrap
+from PIL import Image
+import matplotlib as mpl
+from osgeo import gdal, osr
 
 
-# -- Create a class for calculating the vegetation_index -- #
+# -- Create a class for each vegetation_index -- #
 class Indexes:
 	def __init__(self, img):
 		self.img = img
@@ -16,6 +19,7 @@ class Indexes:
 
 
 	# All these operations aim to not have ZeroDivisionError -- #
+
 	# -- Visible Atmospheric Resistant Index -- #
 	def VARI(self):
 		vari = np.divide((self.G - self.R), (self.G + self.R - self.B + 0.00001))
@@ -33,11 +37,10 @@ class Indexes:
 
 	# -- Normalized Green Blue Difference Index -- #
 	def NGBDI(self): 
-		ngbdi = (self.G - self.B) / (self.G + self.B + 0.00001)
-		ngbdi = np.clip(ngbdi, -1, +1)
-		return ngbdi
+		ngbdi = (self.G - self.B) / (self.G + self.B + 0.00001) 
+		return np.clip(ngbdi, -1, +1)
 
-	# --  Identification of the Idx object -- #
+	# -- Identification of the Idx object -- #
 	def get_index(self, index_name):
 		if index_name == 'VARI':
 			return self.VARI()
@@ -59,16 +62,50 @@ def find_real_min_max(perc, edges, index_clear):
 	max_v = edges[mask].max()
 	return min_v, max_v
 
-# -- A necessary message for the arguments
-parser = argparse.ArgumentParser(epilog = textwrap.dedent('''\
-						        The available VIs are:
-						        1. Visible Atmospheric Resistant Index (VARI)
-						        2. Green Leaf Index (GLI)
-						        3. Normalized Green Red Difference Index (NGRDI)
-						        4. Normalized Green Blue Difference Index (NGBDI)
-						        '''), formatter_class=argparse.RawTextHelpFormatter)
+# -- Function that creates the georeferenced VI map -- #
+def array_to_raster(output_path, ds_reference, array, name):
+	rows, cols, band_num = array.shape
 
-# -- Parsing the args -- #
+	driver = gdal.GetDriverByName("GTiff")
+
+	outRaster = driver.Create(os.path.join(output_path, name+'.tif'), cols, rows, band_num, gdal.GDT_Byte, options=["COMPRESS=DEFLATE"])
+	originX, pixelWidth, b, originY, d, pixelHeight = ds_reference.GetGeoTransform()
+	outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
+
+	descriptions = ['Red Band', 'Green Band', 'Blue Band', 'Alpha Band', 'Index Array']
+	for b in range(band_num):
+		outband = outRaster.GetRasterBand(b+1)
+		outband.WriteArray(array[:,:,b])
+		outband.SetDescription(descriptions[b])
+		if b+1==1:
+			outRaster.GetRasterBand(1).SetColorInterpretation(gdal.GCI_RedBand)
+		elif b+1==2:
+			outRaster.GetRasterBand(2).SetColorInterpretation(gdal.GCI_GreenBand)
+		elif b+1==3:
+			outRaster.GetRasterBand(3).SetColorInterpretation(gdal.GCI_BlueBand)
+		elif b+1==4:
+			outRaster.GetRasterBand(4).SetColorInterpretation(gdal.GCI_AlphaBand)
+		
+	
+	outRasterSRS = osr.SpatialReference(wkt=prj)
+	outRaster.SetProjection(outRasterSRS.ExportToWkt())
+	driver = None
+	outband.FlushCache()
+
+	print('Georeferenced {} map was extracted!'.format(index_name))
+
+	return outRaster
+		
+# -- Arguments -- #	
+parser = argparse.ArgumentParser(prog='index_calculation', description = textwrap.dedent('''\
+								The available VIs are:
+								1. Visible Atmospheric Resistant Index (VARI)
+								2. Green Leaf Index (GLI)
+								3. Normalized Green Red Difference Index (NGRDI)
+								4. Normalized Green Blue Difference Index (NGBDI)
+								'''), formatter_class=argparse.RawTextHelpFormatter)
+
+
 parser.add_argument('--input_image', required=True,
 			  help="Please enter the absolute path of the input image.")
 
@@ -76,7 +113,7 @@ parser.add_argument('--output_path', required=True,
 			  help="Please enter the absolute path of the output path.")
 
 parser.add_argument('--vis', nargs="*", required=False,
-			  help="Please enter the short name of the Vegetation Index/Indices.")
+			  help="Please enter the abbreviation of the Vegetation Index/Indices.")
 
 args = parser.parse_args()
 
@@ -86,47 +123,57 @@ save_dir = os.path.abspath(args.output_path)
 
 if len(args.vis) == 0:
 	args.vis = ['VARI', 'GLI', 'NGRDI', 'NGBDI']
+	print('All VIs will be calculated!')
 else:
 	args.vis = [elem.upper() for elem in args.vis]
 
-img_4ch = cv2.imread(img_name, cv2.IMREAD_UNCHANGED)
-img = img_4ch[:, :, :3].astype(np.float)
-img[img_4ch[:, :, 3] == 0] = np.nan
-empty_space = img_4ch[:, :, 3] == 0
+os.chdir(os.path.dirname(img_path))
+
+img = cv2.imread(img_name, cv2.IMREAD_UNCHANGED)
+h, w, ch = img.shape
+if ch==4:
+	image = img[:, :, :3].astype(float)
+	image[img[:, :, 3] == 0] = np.nan
+	empty_space = img[:, :, 3] == 0
+else:
+	image = img
 
 # -- Print function for testing reasons -- #
 print('Processing image with shape {} x {}'.format(img.shape[0], img.shape[1]))
 
+Idx = Indexes(image)
 
-Idx = Indexes(img)
-
-for i in args.vis:
+for index_name in args.vis:
 	# -- Calculate index -- #
-	idx = Idx.get_index(i)
-
+	idx = Idx.get_index(index_name)
 
 	index_clear = idx[~np.isnan(idx)]
 
 	# -- Calculate index histogram -- #
 	perc, edges, _ = plt.hist(index_clear, bins=100, range=(-1, 1), color='darkcyan', edgecolor='black')
+	plt.close()
 
 	# -- Find the real min, max values of the vegetation_index -- #
 	lower, upper = find_real_min_max(perc, edges, index_clear)
-
-	# -- Plot and save the vegetation_index -- #
-	f = plt.figure()
-	f.set_figheight(idx.shape[0]/f.get_dpi())
-	f.set_figwidth(idx.shape[1]/f.get_dpi())
-	ax = plt.Axes(f, [0., 0., 1., 1.])
-	ax.set_axis_off()
-	f.add_axes(ax)
-	ax.imshow(np.clip(idx, lower, upper), cmap='RdYlGn', aspect='auto')
-	f.savefig('{}/{}.png'.format(save_dir, i), transparent=True)
-	plt.close()
-
-	# -- The *.npy files are useful for the Problematic Areas Detection module -- # 
 	index_clipped = np.clip(idx, lower, upper)
-	np.save('{}/{}_clipped.npy'.format(save_dir, i), index_clipped)
+	
+	cm = plt.get_cmap('RdYlGn')
+	cNorm = mpl.colors.Normalize(vmax=upper, vmin=lower)
+	colored_image = cm(cNorm(index_clipped))
+	img = Image.fromarray(np.uint8(colored_image * 255), mode='RGBA')
+	
+	rgba = np.array(img, dtype=np.float32)
 
-# -- Print function for testing reasons -- #	
+
+	ds = gdal.Open(img_path, gdal.GA_ReadOnly)
+	prj = ds.GetProjection()
+
+	if prj: 
+		array_to_raster(save_dir, ds, rgba, index_name)	
+	else:
+		img.save('{}/{}.png'.format(save_dir, index_name))
+		print('Non georeferrenced {} map was extracted'.format(index_name))
+
+	np.save('{}/{}.npy'.format(save_dir, index_name), index_clipped)
+	
 print('Done!')
