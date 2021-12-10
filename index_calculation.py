65755c1e0f966750350724e83ed049efc16a01d0
giorgos kalomitsinis <giorgoskalomitsinis@gmail.com>
2021-12-10 10:48:48 +0200

    Update index_calculation.py

--- index_calculation.py
+++ index_calculation.py
@@ -61,25 +61,21 @@ def array_to_raster(output_path, ds_reference, array, name):
 
 	driver = gdal.GetDriverByName("GTiff")
 
-	outRaster = driver.Create(os.path.join(output_path, name+'.tif'), cols, rows, band_num, gdal.GDT_Float32, options=["COMPRESS=DEFLATE"])
+	outRaster = driver.Create(os.path.join(output_path, name+'.tif'), cols, rows, band_num, gdal.GDT_Byte, options=["COMPRESS=DEFLATE"])
 	originX, pixelWidth, b, originY, d, pixelHeight = ds_ref.GetGeoTransform()
 	outRaster.SetGeoTransform((originX, pixelWidth, 0, originY, 0, pixelHeight))
 
-	descriptions = ['Red Band', 'Green Band', 'Blue Band', 'Alpha Band', 'Index Array']
 	for b in range(band_num):
 		outband = outRaster.GetRasterBand(b+1)
 		outband.WriteArray(array[:,:,b])
-		outband.SetDescription(descriptions[b])
 		if b+1==1:
 			outRaster.GetRasterBand(1).SetColorInterpretation(gdal.GCI_RedBand)
 		elif b+1==2:
 			outRaster.GetRasterBand(2).SetColorInterpretation(gdal.GCI_GreenBand)
 		elif b+1==3:
 			outRaster.GetRasterBand(3).SetColorInterpretation(gdal.GCI_BlueBand)
-		elif b+1==4:
-			outRaster.GetRasterBand(4).SetColorInterpretation(gdal.GCI_AlphaBand)
 		else:
-			outRaster.GetRasterBand(5).SetColorInterpretation(gdal.GCI_Undefined)
+			outRaster.GetRasterBand(4).SetColorInterpretation(gdal.GCI_AlphaBand)
 		
 	prj = ds_ref.GetProjection()
 	outRasterSRS = osr.SpatialReference(wkt=prj)
@@ -88,21 +84,12 @@ def array_to_raster(output_path, ds_reference, array, name):
 	outband.FlushCache()
 
 	return outRaster
-		
-		
-def winapi_path(dos_path, encoding=None):
-    if (not isinstance(dos_path, str) and encoding is not None): 
-        dos_path = dos_path.decode(encoding)
-    path = os.path.abspath(dos_path)
-    if path.startswith(u"\\\\"):
-        return u"\\\\?\\UNC\\" + path[2:]
-    return u"\\\\?\\" + path
 
 
-img_path = winapi_path(sys.argv[1])
+img_path = sys.argv[1]
 path_1, path_2 = img_path.split('\\docker_stitching\\')
 project_name = path_2.split('\\')[0]
-save_dir = winapi_path(os.path.join(sys.argv[2], project_name))
+save_dir = os.path.join(sys.argv[2], project_name)
 os.makedirs(save_dir, exist_ok=True)
 
 os.chdir(save_dir)
@@ -135,7 +122,7 @@ for index_name in indices_names:
 
 	rgba = np.array(img, dtype=np.float32)
 
-	array_5ch = np.dstack((rgba, index_clipped))
-	array_to_raster(save_dir, img_path, array_5ch, index_name)
+	array_to_raster(save_dir, img_path, rgba, index_name)
+	np.save('{}.npy'.format(index_name), index_clipped)
 	
 print('Done!')
